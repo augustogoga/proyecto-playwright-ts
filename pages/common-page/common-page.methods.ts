7f86a84a562e4b81fa3eb24580a7dadd8d7e658e
fistol <augustogoga@gmail.com>
2025-06-26 17:10:17 -0300

    Add methods to the CommonPageMethods class

--- pages/common-page/common-page.methods.ts
+++ pages/common-page/common-page.methods.ts
@@ -1,13 +1,36 @@
 import {Page} from 'playwright'
+import { CommonPageElements } from './common-page.elements'
 
 export class CommonPageMethods{
     private page: Page
-    
+    private commonPageElements: CommonPageElements
+
     constructor(page: Page){
         this.page = page
+        this.commonPageElements = new CommonPageElements(page)
     }
 
     async navigateToTheApplication(){
         await this.page.goto('https://www.saucedemo.com/v1/index.html')
+    }
+    
+    async openMenu(){
+        await this.commonPageElements.buttons.openMenu.click()
+    }
+
+    async clickOnAllItemsOption(){
+        await this.commonPageElements.leftMenu.allItems.click()
+    }
+
+    async clickOnAboutOption(){
+        await this.commonPageElements.leftMenu.about.click()
+    }
+
+    async clickOnLogOutOption(){
+        await this.commonPageElements.leftMenu.logout.click()
+    }
+
+       async clickOnResetAppStateOption(){
+        await this.commonPageElements.leftMenu.resetAppState.click()
     }
 }
